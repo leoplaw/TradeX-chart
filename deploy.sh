142631e5da0a5e56e8fef35bd152574d8b3d323c
TradeX-app <my.github@tutanota.com>
2022-08-25 21:58:33 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b github
+git checkout -b main
 git add -A
 git commit -m 'deploy'
 
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com/tradex-app/TradeX-chart github:gh-pages
+git push -f git@github.com/tradex-app/TradeX-chart.git main:gh-pages
 
 cd -
